252198701f6bba5cfda780a0ca0623cee5e96e28
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-05 22:10:18 -0600

    Logic

--- Variables_and_Operators.cs
+++ Variables_and_Operators.cs
@@ -1,8 +1,8 @@
-#Variables-containers for storing data values
+//Variables-containers for storing data values
 
-#This is a String variable and it is used to store text and phrases for example "Hello world" or names like "Kevin" 
-#They are also always surrounded by ""
-#When the program is executed it will print out my name Kevin
+//This is a String variable and it is used to store text and phrases for example "Hello world" or names like "Kevin" 
+//They are also always surrounded by ""
+//When the program is executed it will print out my name Kevin
  
 using System;
 
@@ -18,8 +18,8 @@ namespace MyApplication
   }
 }
  
- #Integer Variable- stores whole numbers that aren't decimals like 100 or -100
- #When code is executed it will display myNum value
+ //Integer Variable- stores whole numbers that aren't decimals like 100 or -100
+ //When code is executed it will display myNum value
 
  using System;
 
@@ -35,8 +35,8 @@ namespace MyApplication
   }
 }
 
-#Boolean Variables store values of 2 states true or false
-#When code is executed have options true or false 
+//Boolean Variables store values of 2 states true or false
+//When code is executed have options true or false 
 
 using System;
 
@@ -54,8 +54,8 @@ namespace MyApplication
   }
 }
 
-#Float Variables store floating point numbers or numbers with decimals for example 0.99, 1.99 etc. 
-#When code is executed display myDoubleNum value
+//Float Variables store floating point numbers or numbers with decimals for example 0.99, 1.99 etc. 
+//When code is executed display myDoubleNum value
 
 using System;
 
@@ -71,9 +71,9 @@ namespace MyApplication
   }
 }
 
-#Operators- operators perform operations on values and variables for example adding, subtraction multiplication, etc.
-#Arithmetic Operator-used for common mathmematical operations
-#After code is executed display the value of x which will be 100 after they add 50 and 50 together
+//Operators- operators perform operations on values and variables for example adding, subtraction multiplication, etc.
+//Arithmetic Operator-used for common mathmematical operations
+//After code is executed display the value of x which will be 100 after they add 50 and 50 together
 
 using System;
 
@@ -89,8 +89,8 @@ namespace MyApplication
   }
 }
 
-#Assign Operators-Their purpose to to assign values to variables
-#When this code is executed it will display x which will have the value 5
+//Assign Operators-Their purpose to to assign values to variables
+//When this code is executed it will display x which will have the value 5
 
 using System;
 
@@ -106,8 +106,8 @@ namespace MyApplication
   }
 }
 
-#Comparison Operators-used to compare two values or variables
-#Will display the code as true because the value x is greater than y
+//Comparison Operators-used to compare two values or variables
+//Will display the code as true because the value x is greater than y
 
 using System;
 
@@ -124,3 +124,19 @@ namespace MyApplication
   }
 }
 
+//Logical Operators-can also test true and false like comparison operators but also tests the logic between variables and values
+//When code runs it has two true or false statements and in this case will return true 
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 6;
+      Console.WriteLine(x > 3 && x < 10); // returns True because 6 is greater than 3 AND 6 is less than 10
+     }
+  }
+}
