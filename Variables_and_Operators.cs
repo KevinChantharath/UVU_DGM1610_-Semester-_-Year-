91c1b2f3aaf9fd81fc935d1112d9ae75cb49b4a0
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-05 21:32:08 -0600

    String

--- Variables_and_Operators.cs
+++ Variables_and_Operators.cs
@@ -1,2 +1,22 @@
+#Variables-containers for storing data values
 
+#This is a String variable and it is used to store text and phrases for example "Hello world" or names like "Kevin" 
+#They are also always surrounded by ""
+#When the program is executed it will print out my name Kevin
  
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Kevin";
+      Console.WriteLine(name);  
+     }
+  }
+}
+ 
+ #Integer Variable- stores whole numbers that aren't decimals like 100 or -100
+ #
