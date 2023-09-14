7254acb270102d57f0597f9149232788a5658d7d
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-13 22:59:51 -0600

    Asks for celsius

---
+++ Unit_1b/Unit_1b_Lab.cs
@@ -0,0 +1,16 @@
+// Asks to enter the temperature in celsius
+
+using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the current temperature in celsius");
+            
+            }
+        }
+
+  //
+
+  
