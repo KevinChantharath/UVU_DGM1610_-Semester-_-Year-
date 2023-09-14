f5a3cdfa2982b8b3cd8c8984815c517254cbc8e3
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-13 23:18:08 -0600

    Added if and else statements

--- Unit_1b/Unit_1b_Lab.cs
+++ Unit_1b/Unit_1b_Lab.cs
@@ -1,4 +1,4 @@
-// Asks to enter the temperature in celsius
+
 
 using System;
 
@@ -6,11 +6,24 @@ using System;
     {
         static void Main()
         {
-            Console.WriteLine("Enter the current temperature in celsius");
-            
-            }
-        }
+            Console.WriteLine("Enter the current temperature in celsius"); // Asks to enter the temperature in celsius
 
-  //
+            int temp = 30; //Sets the temperature 
+      if (temp > 30) 
+      {
+        Console.WriteLine("It's hot outside remember to stay hydrated"); //will display this line if greater than 30
+      } 
+      else if (temp < 30) 
+      {
+        Console.WriteLine("Enjoy the Pleasant weather outside"); //will dispaly this line if less than 30
+      } 
+      else 
+      {
+        Console.WriteLine("Perfect Weather"); // displays if they put in 30 exactly 
+      }
+            
+        }
+    }
 
   
+
