17be7bcce87b5a4b8a9f0da42587999189b04bfd
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-15 23:10:43 -0600

    pasted test code

--- Unit_1b/Unit_1b_Challenge.cs
+++ Unit_1b/Unit_1b_Challenge.cs
@@ -1,3 +1,5 @@
+//Enhancing celcius code
+
 using System;
 
     public class Program
@@ -26,3 +28,41 @@ using System;
                 }
         }
 }
+
+//Enhancing Testing Code
+
+ using System;
+    
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your scores in"); // enter your score
+
+            int number = 100;
+            if (number >= 90) 
+      {
+        Console.WriteLine("Excellent Job You're Grade is A"); // display if equal to or higher than 90
+      } 
+      else if (number >= 80 && number <= 89) 
+      {
+      	Console.WriteLine("Great Job You're Grade is B"); // display if equal to or higher than 80-89
+      }
+      else if (number >= 70 && number <= 79)
+      {
+      	Console.WriteLine("Still ok You're Grade is C"); // display if equal to or higher than 70-79
+      }
+      else if (number >= 60 && number <= 69)
+      {
+      	Console.WriteLine("Needs work You're Grade is D"); // display if equal to or higher than 60-69
+      }
+      else
+      {
+      	Console.WriteLine("You need help You're Grade is F"); // display if less than 60 and everything else
+      }
+
+
+        }
+    }
+
+    
