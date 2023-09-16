fd3e698853d48ea2477e8e6667f564e1e9056461
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-15 23:24:48 -0600

    Actually Finished Enhancing Test Code

--- Unit_1b/Unit_1b_Challenge.cs
+++ Unit_1b/Unit_1b_Challenge.cs
@@ -29,44 +29,16 @@ using System;
         }
 }
 
-//Normal test code
-
- using System;
-    
-    public class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Enter your scores in"); // enter your score
-
-            int number = 100;
-            if (number >= 90) 
-      {
-        Console.WriteLine("Excellent Job You're Grade is A"); // display if equal to or higher than 90
-      } 
-      else if (number >= 80 && number <= 89) 
-      {
-      	Console.WriteLine("Great Job You're Grade is B"); // display if equal to or higher than 80-89
-      }
-      else if (number >= 70 && number <= 79)
-      {
-      	Console.WriteLine("Still ok You're Grade is C"); // display if equal to or higher than 70-79
-      }
-      else if (number >= 60 && number <= 69)
-      {
-      	Console.WriteLine("Needs work You're Grade is D"); // display if equal to or higher than 60-69
-      }
-      else
-      {
-      	Console.WriteLine("You need help You're Grade is F"); // display if less than 60 and everything else
-      }
-
-
 
 
 //Enhancing test code asking subject wtih switches
     
+using System;
 
+    public class Program
+{
+	    public void Main()
+	    {   
 
         Console.WriteLine("Enter your favorite subject 1 = math 2 = science 3 = english")// tell them to enter subject using numbers
 
@@ -84,4 +56,4 @@ using System;
           break;
       }
         }
-    }
+}
