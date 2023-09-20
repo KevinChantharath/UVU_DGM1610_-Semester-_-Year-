de598a28920be23f5b9273afd6681f6fb40bd816
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-19 22:02:20 -0600

    idk

--- Unit_1c/1c_lab.cs
+++ Unit_1c/1c_lab.cs
@@ -4,9 +4,9 @@ using System;
   {
     static void Main(string[] args)
     {
-      for (int i = 0; i < 5; i++) 
+      for (int i = 0; i < 5; i++) //puts in integers from 0 to 5
       {
-        Console.WriteLine(i);
+        Console.WriteLine(i); // prints out number 0 to 5
       }    
     }
   }
