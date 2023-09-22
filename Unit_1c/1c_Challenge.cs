57463147e261d60b80b75938eaaede25640367c0
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-22 13:35:41 -0600

    added extra attempts

--- Unit_1c/1c_Challenge.cs
+++ Unit_1c/1c_Challenge.cs
@@ -13,25 +13,31 @@ public class Program
         Console.Write("Guess the number: ");
 		int userGuess = int.Parse(Console.ReadLine());
 		{
-			for(; userGuess != randomNumber; attempt++)
+			for(;userGuess != randomNumber; attempt++)
 			{
 				if(userGuess<randomNumber)
 				{
-					Console.WriteLine("Try guessing higher");
-					break;
+					Console.Write("Try guessing higher");
+					int guess = int.Parse(Console.ReadLine());
+					break;	
 				}
 				
 				else if (userGuess>randomNumber)
 				{
 					Console.WriteLine("Try guessing lower");
-					break;
+					int guess = int.Parse(Console.ReadLine());
+					break;						
 				}
 			}
 			
 			if (userGuess==randomNumber)
 			{
-				Console.WriteLine("You guessed the number! You got it in" + attempt + "attempts");
+				Console.WriteLine("You guessed the number!");
+				
 			}
+		}
+	}
+}
 
             
 
