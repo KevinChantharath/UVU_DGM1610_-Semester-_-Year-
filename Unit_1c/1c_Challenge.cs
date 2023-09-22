1295e6d11c6db6ec8d1615058981a719e776cc6f
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-22 13:38:27 -0600

    finished code for random number generator

--- Unit_1c/1c_Challenge.cs
+++ Unit_1c/1c_Challenge.cs
@@ -6,33 +6,33 @@ public class Program
 {
 	public static void Main()
 	{
-		Random random = new Random();
-        int randomNumber = random.Next(1, 11);
+		Random random = new Random(); //generates random stuff
+        int randomNumber = random.Next(1, 11);//generates random number from 1-10
 		int attempt = 0;
 		
-        Console.Write("Guess the number: ");
-		int userGuess = int.Parse(Console.ReadLine());
+        Console.Write("Guess the number you have 2 attempts: "); //display this quote
+		int userGuess = int.Parse(Console.ReadLine());//gives user input
 		{
 			for(;userGuess != randomNumber; attempt++)
 			{
 				if(userGuess<randomNumber)
 				{
-					Console.Write("Try guessing higher");
-					int guess = int.Parse(Console.ReadLine());
-					break;	
+					Console.Write("Try guessing higher"); //if number is less than generated number display this
+					int guess = int.Parse(Console.ReadLine()); //gives extra attempt
+					break;
 				}
 				
 				else if (userGuess>randomNumber)
 				{
-					Console.WriteLine("Try guessing lower");
-					int guess = int.Parse(Console.ReadLine());
+					Console.WriteLine("Try guessing lower");//if number is greater than generated number display this
+					int guess = int.Parse(Console.ReadLine()); // gives extra attempt
 					break;						
 				}
 			}
 			
 			if (userGuess==randomNumber)
 			{
-				Console.WriteLine("You guessed the number!");
+				Console.WriteLine("You guessed the number!"); //display if number is guessed correctly
 				
 			}
 		}
