65e431f5649a720be11c536b00c57be331a41242
KevinChantharath <143042087+KevinChantharath@users.noreply.github.com>
2023-09-22 13:03:55 -0600

    Added attempts and guess attempts

--- Unit_1c/1c_Challenge.cs
+++ Unit_1c/1c_Challenge.cs
@@ -1,9 +1,25 @@
 //Code for Random Number Generator
 
-using System;
-					
+using System:
+
 public class Program
 {
 	public static void Main()
 	{
+		Random random = new Random();
+        int randomNumber = random.Next(1, 11);
+		int attempt = 0;
+		
         Console.Write("Guess the number: ");
+		int userGuess = int.Parse(Console.ReadLine());
+		{
+			for(; userGuess != randomNumber; attempt++)
+			{
+				if(userGuess<randomNumber)
+				{
+					Console.WriteLine("Try guessing higher");
+					break;
+				}
+
+
+
